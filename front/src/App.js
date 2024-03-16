76be3014c8ac94a4e9a0eccd8eb1327b717daa40
skoyama@joolen.com <skoyama@joolen.com>
2024-03-16 12:27:09 +0900

    front npmを削除してみた

--- front/src/App.js
+++ front/src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div>
       {/*<h1>User List</h1>*/}
-      <h1>User List GitHub Actions Test v5</h1>
+      <h1>User List GitHub Actions Test v6</h1>
       <ul>
         {users.map((user) => (
           <li>
