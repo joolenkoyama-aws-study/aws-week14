a2a54fff2f094e1b46c811da830bd87a40ed30ed
skoyama@joolen.com <skoyama@joolen.com>
2024-03-30 09:36:42 +0900

    修正

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,7 +18,7 @@ Route::get('/dummy-data', function () {
     $data = [
         [
 //            'name' => 'John Doe',
-            'name' => 'Michael Andrew Fox3',
+            'name' => 'Michael Andrew Fox4',
             'email' => 'john.doe@example.com',
             'age' => 30,
         ],
