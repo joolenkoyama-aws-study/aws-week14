4eb91e011c56e86e159c4c2f4a467b6488eab9f1
skoyama@joolen.com <skoyama@joolen.com>
2024-04-05 17:35:09 +0900

    16週の修正

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,7 +18,7 @@ Route::get('/dummy-data', function () {
     $data = [
         [
 //            'name' => 'John Doe',
-            'name' => 'Michael Andrew Fox4',
+            'name' => 'Michael Andrew Fox5',
             'email' => 'john.doe@example.com',
             'age' => 30,
         ],
@@ -34,4 +34,9 @@ Route::get('/dummy-data', function () {
         ],
     ];
     return response()->json($data);
+});
+
+// 16週で追加
+Route::get('/users', function() {
+    return User::all();
 });
