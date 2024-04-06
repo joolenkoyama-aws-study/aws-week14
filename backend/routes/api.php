60bed1160ba07cb8fa34463e12d110c284cc599f
skoyama@joolen.com <skoyama@joolen.com>
2024-04-06 10:40:34 +0900

    backend/routes/api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,7 +18,7 @@ Route::get('/dummy-data', function () {
     $data = [
         [
 //            'name' => 'John Doe',
-            'name' => 'Michael Andrew Fox5',
+            'name' => 'Michael Andrew Fox16-1',
             'email' => 'john.doe@example.com',
             'age' => 30,
         ],
@@ -37,6 +37,6 @@ Route::get('/dummy-data', function () {
 });
 
 // 16週で追加
-//Route::get('/users', function() {
-//    return User::all();
-//});
+Route::get('/users', function() {
+    return User::all();
+});
